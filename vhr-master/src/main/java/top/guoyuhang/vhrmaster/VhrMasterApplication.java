6b2f8333e047a4dd063615ce0c8eba70a628acd5
peinimogu <1131717341@qq.com>
2021-01-03 14:57:18 +0800

    create

---
+++ vhr-master/src/main/java/top/guoyuhang/vhrmaster/VhrMasterApplication.java
@@ -0,0 +1,13 @@
+package top.guoyuhang.vhrmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrMasterApplication.class, args);
+    }
+
+}
